57db6c77b364d7b9befd83336637d37d62fe828d
Tarik Wesley <tarikwesley@gmail.com>
2024-06-19 18:18:59 -0300

    feat: add domains

---
+++ src/main/kotlin/com/devturing/forum/model/StatusTopico.kt
@@ -0,0 +1,8 @@
+package com.devturing.forum.model
+
+enum class StatusTopico {
+    NAO_RESPONDIDO,
+    NAO_SOLUCIONADO,
+    SOLUCIONADO,
+    FECHADO
+}
---
+++ src/main/kotlin/com/devturing/forum/model/Topico.kt
@@ -0,0 +1,14 @@
+package com.devturing.forum.model
+
+import java.time.LocalDateTime
+
+data class Topico(
+    val id: Long? = null,
+    val titulo: String,
+    val mensagem: String,
+    val dataCriacao: LocalDateTime = LocalDateTime.now(),
+    val curso: Curso,
+    val usuario: Usuario,
+    val status: StatusTopico = StatusTopico.NAO_RESPONDIDO,
+    val respostas: List<Resposta> = ArrayList()
+)
