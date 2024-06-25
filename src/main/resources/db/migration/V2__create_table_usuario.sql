b367120dff8a5248e2edf2ff1b287f58773d55a0
Tarik Wesley <tarikwesley@gmail.com>
2024-06-25 18:52:17 -0300

    feat: add flyway and migrations

---
+++ src/main/resources/db/migration/V2__create_table_usuario.sql
@@ -0,0 +1,9 @@
+create table usuario
+(
+    id    bigint not null auto_increment,
+    nome  varchar(50) not null,
+    email varchar(50) not null,
+    primary key (id)
+);
+
+insert into usuario values (1, 'Nath', 'nath@email.com');
